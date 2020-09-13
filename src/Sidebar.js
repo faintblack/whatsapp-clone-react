6db241df73176499900599b5f89d733e3e1096e5
faintblack <mahardikakharisma@gmail.com>
2020-09-13 14:59:49 +0700

    sidebar chat

---
+++ src/Sidebar.js
@@ -0,0 +1,40 @@
+import { Avatar, IconButton } from '@material-ui/core';
+import { Chat, DonutLarge, MoreVert, SearchOutlined } from '@material-ui/icons';
+import React from 'react';
+import './Sidebar.css';
+import SidebarChat from './SidebarChat';
+
+function Sidebar() {
+	return (
+		<div className="sidebar">
+			<div className="sidebar-header">
+				<Avatar />
+				<div className="sidebar-header-right">
+					<IconButton>
+						<DonutLarge />
+					</IconButton>
+					<IconButton>
+						<Chat />
+					</IconButton>
+					<IconButton>
+						<MoreVert />
+					</IconButton>
+				</div>
+			</div>
+			<div className="sidebar-search">
+				<div className="sidebar-search-container">
+					<SearchOutlined />
+					<input placeholder="Search or start a new chat" />
+				</div>
+			</div>
+			<div className="sidebar-chat">
+                <SidebarChat/>
+                <SidebarChat/>
+                <SidebarChat/>
+                <SidebarChat/>
+            </div>
+		</div>
+	);
+}
+
+export default Sidebar;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+	apiKey: 'AIzaSyDWNPmNQ4pSHGlAyxihvYzo3g1weAaLM_c',
+	authDomain: 'whatsapp-clone-d7243.firebaseapp.com',
+	databaseURL: 'https://whatsapp-clone-d7243.firebaseio.com',
+	projectId: 'whatsapp-clone-d7243',
+	storageBucket: 'whatsapp-clone-d7243.appspot.com',
+	messagingSenderId: '628994623853',
+	appId: '1:628994623853:web:7c4a478c02a499b5bd3264',
+	measurementId: 'G-SW1ZJN618G'
+};
